54dccecddc7fa2b2e273d1f0a8efed10a5983bf4
JINNY <103866627+yeyae@users.noreply.github.com>
2022-07-08 20:39:21 +0900

    Update 19.py

--- 19.py
+++ 19.py
@@ -15,3 +15,21 @@ list(a.keys())
 
 #Value 리스트 만들기 (values)
 a.values()
+
+#Key,Value 쌍 얻기(items)
+a.items()
+#Key,Value 쌍 모두 지우기(clear)
+a.clear()
+a
+#Key로 Value 얻기(get)
+a = {'name' : jjj, 'phone' : '01012345678', 'birth' : '0111'}
+a.get('name')
+a.get('phone')
+
+#딕셔너리에서 찾는 값이 없을 경우 미리 정해 둔 디폴트값을 대신 가져오고 싶을 때: get(x, '디폴트값')
+a.get('foo', 'bar')
+
+#해당 Key가 딕셔너리 안에 있는지 조사하기(in)
+a = {'name' : jjj, 'phone' : '01012345678', 'birth' : '0111'}
+'name' in a
+'email' in a
